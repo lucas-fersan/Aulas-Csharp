8b63224adc135721e76cbb48117129f7f543e2b9
lucas-fersan <lucasfersan182@gmail.com>
2025-02-25 20:10:39 -0300

    boa noite

---
+++ aula1/AULA01/AULA01/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Hello, \n World!");
+
+Console.Write("lucas\n");
+
+Console.Write("fernandes");
+
+string firstName = "lucas";
+
+string lastName = "fernandes";
+
+string surname = "santana";
+
+Console.WriteLine("meu nome completo é "+ firstName +" "+" "+ surname + lastName); 
+
+int idade = 19;
+
+Console.WriteLine($"Meu nome completo e 0{firstName} [surname) {lastName) e a minha idade é [idade}");
+ 
+Console.WriteLine(
+string nome = Console.ReadLine());
+Console.WriteLine($' ola {nom
---
+++ aula1/aula1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("lucas\n");
+Console.WriteLine("fernandes \n");
+
+string nomeCompleto = "lucas fernandes santana";
+string primeiroNome = "fernandes";
+string ultimoNome = "santana";
+string nomeMeio = "fernandes santana";
+
+Console.WriteLine("Meu nome é " + primeiroNome + nomeMeio + ultimoNome);
+
+int idade = 19;
+
+Console.WriteLine($"Meu nome é   {primeiroNome}  {nomeMeio} {ultimoNome} e minha idade é {idade}");
+Console.WriteLine("Escreva o seu Nome  ");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Ola {nome}, Boa noite !");
